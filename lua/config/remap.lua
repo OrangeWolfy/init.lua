5a1234b235517d057950d1defb4c6c264a84a18a
OrangeWolfy <orange007wolfy@gmail.com>
2025-02-18 23:18:43 -0600

    Updated keymaps

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,9 +1,6 @@
 -- Remove lookup highlight
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
--- Override new line on normal mode to stay in normal mode
-vim.keymap.set("n", "<o>", "o<Esc>")
-
 -- Duplicate line up or down to the cursor
 vim.keymap.set("n", "<A-S-k>", "yyp")
 vim.keymap.set("n", "<A-S-up>", "yyp")
@@ -16,13 +13,19 @@ vim.keymap.set("n", "<A-up>", "<cmd>m -2<CR>")
 vim.keymap.set("n", "<A-j>", "<cmd>m +1<CR>")
 vim.keymap.set("n", "<A-down>", "<cmd>m +1<CR>")
 
--- Open file browser
-vim.keymap.set("n", "<Leader>e", ":Lex 20<cr>")
-vim.keymap.set("n", "<Leader>r", "<C-o><cr>")
+-- Create window or tab
+vim.keymap.set("n", "<Leader>h", "<C-w>s")
+vim.keymap.set("n", "<Leader>v", "<C-w>v")
+vim.keymap.set("n", "<Leader>t", ":Tex<CR>")
 
--- Move neovim windows
+-- Move between neovim windows and buffers
 vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<A-l>", ":bnext<CR>")
+vim.keymap.set("n", "<A-h>", ":bprev<CR>")
 
+-- Remove windows or buffers
+vim.keymap.set("n", "<Leader>o", "<C-w>o")
+vim.keymap.set("n", "<A-c>", ":bd<CR>")
